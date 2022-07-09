929060e54d415c5b7c4dde464101df006cab8e3e
Jesús Millán <jgmc3012@gmail.com>
2022-07-09 17:34:57 -0400

    :bug: Semaphore and run was using different loops

--- toshl_manager/controllers/loans/clean.py
+++ toshl_manager/controllers/loans/clean.py
@@ -3,7 +3,8 @@ from config import LOAND_CATEGORY_ID, SEPARATOR, TOSH_SECRET_KEY, UNPAYMENT_TAG_
 from services.toshl_finances.entries import types
 from services.toshl_finances.toshl_app import ToshlApp
 from toshl_manager.utils.formatters.loans import LoansFormatter
-from asyncio import Semaphore, gather
+from asyncio import Semaphore, gather, events
+from copy import deepcopy
 
 toshl_app = ToshlApp(TOSH_SECRET_KEY)
 
@@ -12,12 +13,13 @@ class CleanLoansController:
     def __init__(self, from_date, to_date) -> None:
         self._from_date = from_date
         self._to_date = to_date
-        self._sem = Semaphore(3)
+        self.__sem = None
 
     async def execute(self):
         data = await self._entries_list()
+        response = deepcopy(data)
         await gather(*map(self._remove_unpayment_tag, data))
-        return LoansFormatter.format(data)
+        return LoansFormatter.format(response)
 
     def _entries_list(self):
         return toshl_app.entries().list(
@@ -29,7 +31,12 @@ class CleanLoansController:
         )
 
     async def _remove_unpayment_tag(self, entry):
-        data = entry.copy()
-        data["tags"] = list(set(data["tags"]) - {UNPAYMENT_TAG_ID})
+        entry["tags"] = list(set(entry["tags"]) - {UNPAYMENT_TAG_ID})
         async with self._sem:
-            await toshl_app.entries().update(**data)
+            await toshl_app.entries().update(**entry)
+
+    @property
+    def _sem(self):
+        if not self.__sem:
+            self.__sem = Semaphore(3, loop=events.get_event_loop())
+        return self.__sem
