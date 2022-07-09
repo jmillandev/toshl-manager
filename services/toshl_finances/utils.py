70ae271cad617cf8fca43c124acb2506e01cfb85
Jesús Millán <jgmc3012@gmail.com>
2022-07-09 15:10:25 -0400

    :star: Change date to UTC

--- services/toshl_finances/utils.py
+++ services/toshl_finances/utils.py
@@ -1,6 +1,8 @@
 from datetime import datetime
-
 from config import SEPARATOR
+import pytz
+
+local = pytz.timezone("America/Caracas")
 
 def has_containt_all_tags(tag_filters, data):
     tag_filters = set(tag_filters.split(SEPARATOR))
@@ -12,7 +14,9 @@ def has_containt_all_tags(tag_filters, data):
 
 def utc_date(string_date):
     date_time = datetime.strptime(string_date, '%d/%m/%y')
-    return date_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+    local_dt = local.localize(date_time, is_dst=None)
+    utc_dt = local_dt.astimezone(pytz.utc)
+    return utc_dt.strftime('%Y-%m-%dT%H:%M:%SZ')
 
 def optional_params(Endpoint, kwargs):
     return { key:kwargs.get(key) for key in Endpoint.OPTIONAL_PARAMS if kwargs.get(key) != None }
