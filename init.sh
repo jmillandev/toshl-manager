fbbc72089fd374897796cd3f1e64dea0551a3362
Jesús Millán <jgmc3012@gmail.com>
2022-11-24 18:33:22 -0400

    :book: Update Docs

--- init.sh
+++ init.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 
 usage() {
-cat << EOF
+cat << END
 Application Entrypoint.
 
 Usage: ./init COMMAND-DOCKER-COMPOSE
 
 Options:
-  -e <enviroment> (test|local) : Current enviroment
-                                  (default: local)  
-EOF
+  -e (test|local) : Current enviroment
+                    (default: local)  
+END
+exit 0
 }
 
 while getopts 'he:' OPTION; do
   case "$OPTION" in
     e)
       enviroment="$OPTARG"
-      echo "The value provided is $OPTARG"
       ;;
     h)
       usage
-      exit 0
       ;;
   esac
 done
@@ -28,11 +27,11 @@ enviroment=${enviroment:-local}
 
 case $enviroment in
 'local'|'test')
-    echo "Running Script in $enviroment enviroment"
+    echo "Running Script in '$enviroment' enviroment"
     ;;
 *)
-    echo "'$enviroment' is a invalid enviroment. Only 'local' and 'test' are valids"
-    exit 1
+    echo "'$enviroment' is a invalid <enviroment>. Please read de option to -e <enviroment>\n"
+    usage
     ;;
 esac
 
